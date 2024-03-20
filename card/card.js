dbc122ac03fdf28fe0e02ae68d97d4274e2a6d27
geoffrey184 <geoffreynono@icloud.com>
2024-03-20 13:54:03 +0100

    📦 NEW: add cards

--- card/card.js
+++ card/card.js
@@ -3,6 +3,7 @@ const jsonData = [
     {
       name: "Geoffrey Noel",
       jobTitle: "Apprenti CDA",
+      age: "22 ans",
       image: "image/moi image.jpg",
       about: "Bonjour je suis Geoffrey Noel et je suis un apprenti CDA",
       hobbies: "Basket-ball",
@@ -10,6 +11,12 @@ const jsonData = [
         { year: "2023", title: "Formation Simplon", desc: "Développeur intégrateur" },
         { year: "2023", title: "Formation Simplon dwwm", desc: "Développeur Web et Web Mobile" },
         { year: "2024", title: "Formation Simplon CDA", desc: "Concepteur développeur d'applications" }
+      ],
+      stacks: [
+        { 
+          images: ["image/stack1.jpg", "image/stack2.jpg", "image/stack3.jpg"]
+        },
+        // Ajoutez d'autres stacks si nécessaire
       ],
       contact: {
         location: "Marquise",
@@ -23,6 +30,7 @@ const jsonData = [
      {
       name: "Theo Duflos",
       jobTitle: "Apprenti CDA",
+      age: "22 ans",
       image: "image/theo-duflos.jpg",
       about: "Bonjour je suis Theo Duflosl et je suis un apprenti CDA",
       hobbies: "videos games, cinema, animé",
@@ -44,6 +52,7 @@ const jsonData = [
     {
         name: "Theo Couvelard",
         jobTitle: "Apprenti CDA",
+        age: "21 ans",
         image: "image/theocouvelard.webp",
         about: "Bonjour je suis Theo Couvelard et je suis un apprenti CDA",
         hobbies: "musculation, jeux vidéos",
@@ -64,6 +73,7 @@ const jsonData = [
       {
         name: "Karl Gavois ",
         jobTitle: "Apprenti CDA",
+        age: "23 ans",
         image: "image/karl.webp",
         about: "Bonjour je suis Karl Gavois et je suis un apprenti CDA",
         hobbies: "Jeu vidéal / Formule 1 ",
@@ -84,6 +94,7 @@ const jsonData = [
       {
         name: "Alex Buffet",
         jobTitle: "Apprenti CDA",
+        age: "20 ans",
         image: "image/alexbuffet.webp",
         about: "L'amour gagne toujours a la fin",
         hobbies: "jeux videos, la poésie",
@@ -104,7 +115,8 @@ const jsonData = [
       {
         name: "Kévin LEDEZ",
         jobTitle: "Apprenti CDA",
-        image: "image/moi image.jpg",
+        age: "35 ans",
+        image: "image/kevinledez.jpg",
         about: "Bonjour je suis Kévin LEDEZ et je suis un apprenti CDA",
         hobbies: "Jeux, Programmation, Musique",
         experiences: [
@@ -124,7 +136,8 @@ const jsonData = [
       {
         name: "Jean-Baptiste Lavisse",
         jobTitle: "Apprenti CDA",
-        image: "image/moi image.jpg",
+        age: "33 ans",
+        image: "image/jbl.jpg",
         about: "Bonjour je suis Jean-Baptiste Lavisse et je suis un apprenti CDA",
         hobbies: "Piano, Cinéma",
         experiences: [
@@ -144,7 +157,8 @@ const jsonData = [
       {
         name: "William Truant",
         jobTitle: "Apprenti CDA",
-        image: "image/moi image.jpg",
+        age: "20 ans",
+        image: "image/will.jpg",
         about: "Bonjour je suis William Truant et je suis un apprenti CDA",
         hobbies: "Jeu vidéo",
         experiences: [
@@ -164,6 +178,7 @@ const jsonData = [
       {
         name: "Eric Da Silva Rocha",
         jobTitle: "Apprenti CDA",
+        age: "28 ans",
         image: "image/eric.png",
         about: "On ne repousse pas ses limites, on les découvre.",
         hobbies: "Jeu vidéo",
@@ -184,7 +199,8 @@ const jsonData = [
       {
         name: "Vivien Gajac",
         jobTitle: "Apprenti CDA",
-        image: "image/moi image.jpg",
+        age: "38 ans",
+        image: "image/vivien.jpg",
         about: "Bonjour je suis Vivien Gajac et je suis un apprenti CDA",
         hobbies: "Jeux vidéos, Guitare, Lever de coude",
         experiences: [
@@ -204,7 +220,8 @@ const jsonData = [
       {
         name: "Alexandre Merlin",
         jobTitle: "Apprenti CDA",
-        image: "image/moi image.jpg",
+        age: "31 ans",
+        image: "image/Merlin_Alexandre.webp",
         about: "Bonjour je suis Alexandre Merlin et je suis un apprenti CDA",
         hobbies: " jeux vidéos, peinture",
         experiences: [
@@ -222,10 +239,11 @@ const jsonData = [
       },
 
       {
-        name: "",
+        name: "Nicolas Herbez",
         jobTitle: "Apprenti CDA",
-        image: "image/moi image.jpg",
-        about: "Bonjour je suis Geoffrey Noel et je suis un apprenti CDA",
+        age: "25 ans",
+        image: "image/nicoh.jpg",
+        about: "Bonjour je suis Nicolas Herbez et je suis un apprenti CDA",
         hobbies: "",
         experiences: [
           { year: "2023", title: "Formation Simplon", desc: "Développeur intégrateur" },
@@ -233,7 +251,7 @@ const jsonData = [
           { year: "2024", title: "Formation Simplon CDA", desc: "Concepteur développeur d'applications" }
         ],
         contact: {
-          location: "",
+          location: "Samer",
           phone: "",
           email: "",
           linkedin: "",
@@ -244,7 +262,8 @@ const jsonData = [
       {
         name: "Laurie Barsby",
         jobTitle: "Apprenti CDA",
-        image: "image/moi image.jpg",
+        age: "25 ans",
+        image: "image/lbars.jpg",
         about: "Bonjour je suis Laurie Barsby et je suis un apprenti CDA",
         hobbies: "?",
         experiences: [
@@ -257,26 +276,6 @@ const jsonData = [
           phone: "",
           email: "",
           linkedin: "https://www.linkedin.com/in/barsby-laurie-62100/",
-          github: "",
-        }
-      },
-
-      {
-        name: "",
-        jobTitle: "Apprenti CDA",
-        image: "image/moi image.jpg",
-        about: "Bonjour je suis Geoffrey Noel et je suis un apprenti CDA",
-        hobbies: "",
-        experiences: [
-          { year: "2023", title: "Formation Simplon", desc: "Développeur intégrateur" },
-          { year: "2023", title: "Formation Simplon dwwm", desc: "Développeur Web et Web Mobile" },
-          { year: "2024", title: "Formation Simplon CDA", desc: "Concepteur développeur d'applications" }
-        ],
-        contact: {
-          location: "",
-          phone: "",
-          email: "",
-          linkedin: "",
           github: "",
         }
       },
@@ -295,15 +294,16 @@ const jsonData = [
   
       cardDiv.innerHTML = `
         <div class="card-header">
-          <div class="card-cover" style="background-image: url('${person.image}')"></div>
-          <img class="card-avatar" src="${person.image}" alt="avatar" />
-          <h1 class="card-fullname">${person.name}</h1>
-          <h2 class="card-jobtitle">${person.jobTitle}</h2>
+        <div class="card-cover" style="background-image: url('${person.image}')"></div>
+        <img class="card-avatar" src="${person.image}" alt="avatar" />
+        <h1 class="card-fullname">${person.name}</h1>
+        <h3 class="age">${person.jobTitle}</h3>
+        <h2 class="card-jobtitle">${person.age}</h2>
         </div>
         <div class="card-main">
-          <div class="card-section is-active" id="about">
-            <div class="card-content">
-              <div class="card-subtitle">ABOUT</div>
+        <div class="card-section is-active" id="about">
+        <div class="card-content">
+        <div class="card-subtitle">ABOUT</div>
               <p class="card-desc">${person.about}</p>
             </div>
             <div class="card-content">
@@ -321,8 +321,10 @@ const jsonData = [
                     <div class="card-item-title">${exp.title}</div>
                     <div class="card-item-desc">${exp.desc}</div>
                   </div>
+                  
                 `).join('')}
-              </div>
+                </div>
+                <div class="card-subtitle">Stacks</div>
             </div>
           </div>
           <div class="card-section" id="contact">
@@ -335,17 +337,8 @@ const jsonData = [
                     <circle cx="12" cy="10" r="3" /></svg>
                   ${person.contact.location}
                 </div>
-                <div class="card-contact">
-                  <svg xmlns="http://www.w3.org/2000/svg" viewbox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-                    <path d="M22 16.92v3a2 2 0 01-2.18 2 19.79 19.79 0 01-8.63-3.07 19.5 19.5 0 01-6-6 19.79 19.79 0 01-3.07-8.67A2 2 0 014.11 2h3a2 2 0 012 1.72 12.84 12.84 0 00.7 2.81 2 2 0 01-.45 2.11L8.09 9.91a16 16 0 006 6l1.27-1.27a2 2 0 012.11-.45 12.84 12.84 0 002.81.7A2 2 0 0122 16.92z" /></svg>
-                  ${person.contact.phone}
-                </div>
-                <div class="card-contact">
-                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-                    <path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z" />
-                    <path d="M22 6l-10 7L2 6" /></svg>
-                  ${person.contact.email}
-                </div>
+                
+               
                 <div class="card-contact">
                 <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="100" height="100" viewBox="0 0 50 50">
                 <path d="M 9 4 C 6.2504839 4 4 6.2504839 4 9 L 4 41 C 4 43.749516 6.2504839 46 9 46 L 41 46 C 43.749516 46 46 43.749516 46 41 L 46 9 C 46 6.2504839 43.749516 4 41 4 L 9 4 z M 9 6 L 41 6 C 42.668484 6 44 7.3315161 44 9 L 44 41 C 44 42.668484 42.668484 44 41 44 L 9 44 C 7.3315161 44 6 42.668484 6 41 L 6 9 C 6 7.3315161 7.3315161 6 9 6 z M 14 11.011719 C 12.904779 11.011719 11.919219 11.339079 11.189453 11.953125 C 10.459687 12.567171 10.011719 13.484511 10.011719 14.466797 C 10.011719 16.333977 11.631285 17.789609 13.691406 17.933594 A 0.98809878 0.98809878 0 0 0 13.695312 17.935547 A 0.98809878 0.98809878 0 0 0 14 17.988281 C 16.27301 17.988281 17.988281 16.396083 17.988281 14.466797 A 0.98809878 0.98809878 0 0 0 17.986328 14.414062 C 17.884577 12.513831 16.190443 11.011719 14 11.011719 z M 14 12.988281 C 15.392231 12.988281 15.94197 13.610038 16.001953 14.492188 C 15.989803 15.348434 15.460091 16.011719 14 16.011719 C 12.614594 16.011719 11.988281 15.302225 11.988281 14.466797 C 11.988281 14.049083 12.140703 13.734298 12.460938 13.464844 C 12.78117 13.19539 13.295221 12.988281 14 12.988281 z M 11 19 A 1.0001 1.0001 0 0 0 10 20 L 10 39 A 1.0001 1.0001 0 0 0 11 40 L 17 40 A 1.0001 1.0001 0 0 0 18 39 L 18 33.134766 L 18 20 A 1.0001 1.0001 0 0 0 17 19 L 11 19 z M 20 19 A 1.0001 1.0001 0 0 0 19 20 L 19 39 A 1.0001 1.0001 0 0 0 20 40 L 26 40 A 1.0001 1.0001 0 0 0 27 39 L 27 29 C 27 28.170333 27.226394 27.345035 27.625 26.804688 C 28.023606 26.264339 28.526466 25.940057 29.482422 25.957031 C 30.468166 25.973981 30.989999 26.311669 31.384766 26.841797 C 31.779532 27.371924 32 28.166667 32 29 L 32 39 A 1.0001 1.0001 0 0 0 33 40 L 39 40 A 1.0001 1.0001 0 0 0 40 39 L 40 28.261719 C 40 25.300181 39.122788 22.95433 37.619141 21.367188 C 36.115493 19.780044 34.024172 19 31.8125 19 C 29.710483 19 28.110853 19.704889 27 20.423828 L 27 20 A 1.0001 1.0001 0 0 0 26 19 L 20 19 z M 12 21 L 16 21 L 16 33.134766 L 16 38 L 12 38 L 12 21 z M 21 21 L 25 21 L 25 22.560547 A 1.0001 1.0001 0 0 0 26.798828 23.162109 C 26.798828 23.162109 28.369194 21 31.8125 21 C 33.565828 21 35.069366 21.582581 36.167969 22.742188 C 37.266572 23.901794 38 25.688257 38 28.261719 L 38 38 L 34 38 L 34 29 C 34 27.833333 33.720468 26.627107 32.990234 25.646484 C 32.260001 24.665862 31.031834 23.983076 29.517578 23.957031 C 27.995534 23.930001 26.747519 24.626988 26.015625 25.619141 C 25.283731 26.611293 25 27.829667 25 29 L 25 38 L 21 38 L 21 21 z"></path>
